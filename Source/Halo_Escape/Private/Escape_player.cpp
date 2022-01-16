53b2d777ebe398e9be7100f392fe302b059d8087
pastawithnosauce <firedragoon105@gmail.com>
2022-01-16 15:48:28 -0500

    Update

--- Source/Halo_Escape/Private/Escape_player.cpp
+++ Source/Halo_Escape/Private/Escape_player.cpp
@@ -2,12 +2,15 @@
 
 
 #include "Escape_player.h"
+#include "Animation/AnimInstance.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
-#include "Animation/AnimInstance.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/InputSettings.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
 #include "Kismet/GameplayStatics.h"
+
+
 
 
 // Sets default values
@@ -64,10 +67,22 @@ void AEscape_player::BeginPlay()
 
 void AEscape_player::MoveForward(float Val)
 {
+	float Value = Val;
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
 }
 
 void AEscape_player::MoveRight(float Val)
 {
+	float Value = Val;
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), Value);
+	}
 }
 
 void AEscape_player::TurnAtRate(float Rate)
@@ -122,4 +137,3 @@ bool AEscape_player::dosomething()
 void AEscape_player::OnFire()
 {
 }
-
